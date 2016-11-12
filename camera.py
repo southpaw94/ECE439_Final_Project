e246e4244d9c1ba5f1dacf054d2d75f6da5594df
wdedds <wdedds@oakland.edu>
2016-11-12 19:01:03 +0000

    camera.py as of 2:15 pm, Sunday

--- camera.py
+++ camera.py
@@ -2,118 +2,105 @@
 import cv2
 import math
 import numpy as np
+import pandas as pd
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
 
+# global variables
 angle_array = []
+pix_list = []
+pen_down = False
+image_processed = False
+current_pix = []
+angles_df = pd.DataFrame()
+
 GPIO.setmode(GPIO.BCM)
 # universal setup for GPIO pin numbering
 
-# feeds pixel coords to pix_to_ik to output servo angles
-# finds non-zero value in image, goes to that point, searches
-# 3x3 neighborhood, deletes previous point, and repeats
-def get_pix():
 
-	camera = PiCamera()
-	camera.start_preview()
-	sleep(5)
-	camera.capture('picture.jpg')
-	camera.stop_preview()
-	image_captured = cv2.imread('picture.jpg', 0)
-	image_edged = cv2.Canny(image_captured, 125, 200)
+def find_pix( ):
 
-	cv2.imshow('original image', image_captured)
-	cv2.imshow('image with edges', image_edged)
-	height, width = image_edged.shape
-
-	# print image size
-	print "image height = ", height
-	print "image width = ", width
-	# image will be current image that's drawn & modified
-	# image = lana_edged
-	image = image_edged
-	pix_list = []
+	# check if first time through to process image once, otherwise skip this step
+	global image
+	global pix_list
+	global pen_down
+	global image_processed
+	global current_pix
+	global height, width
 	
+	if image_processed == False:
+		#image = saved image that we want processed/drawn
+		image = 'black_line.jpg'
+		image = cv2.imread(image)
+		cv2.imshow('Saved Original', image)
+		image = cv2.Canny(image, 230, 240)
+		cv2.imshow('Saved Edges', image)
+	        height, width = image.shape
+       		print "image height = ", height
+       		print "image width = ", width
+		image_processed = True	
+
 	for i in range(1, height):
 		for j in range(1, width):
-			if image[i,j] != 0:
-				current_pix = [i, j]
+			if image[i, j] != 0:
+				current_pix = [i, j, pen_down]
 				pix_list.append(current_pix)
 				# delete already drawn pixels
 				image[i, j] = 0
 				# make sure we're in the boundaries of the image
-				if ((i-1 > 0) and (j-1 > 0) and (i < height-1) and (j < width-1)):
+				if ((i-1 > 0) and (j-1 > 0) and (i < height) and (j < width)):
 					# check neighborhood (3x3)
+					# if white pixel is in neighborhood, call follow_pix( ) with current location
 					if (image[i-1,j-1]!=0 or image[i-1,j]!=0 or image[i-1,j+1]!=0 or \
 					image[i,j-1]!=0 or image[i,j+1]!=0 or image[i+1,j-1]!=0 or \
 					image[i+1,j]!=0 or image[i+1,j+1]!=0): 
-				  		# index through rows and columns of neighborhood matrix (3x3)
-						# current pixel here is image(k, l)
-						for k in range(i-1, i+2):
-							for l in range(j-1, j+2):
-								if image[k, l] != 0:
-									current_pix = [k, l]
-									pix_list.append(current_pix)
-								        image[k, l] = 0
-									
-	print "i = ", i
-	print "j = ", j
+						follow_pix(i, j)
+					else:
+						# if no white pixels in neighborhood, lift pen up until next pixel is found
+						pen_down = False						
+
 	print "length of pix_list = ", len(pix_list)
-	print "pix_list = ", pix_list
-#	cv2.imshow("final image", image)
+	for p in range(0, len(pix_list)):
+		px_input = pix_list[p][1]
+		py_input = pix_list[p][0]
+		pen_state = pix_list[p][2]
+		pix_to_ik(px_input, py_input, pen_state)
 
 
 
-def get_pix_saved_image(im_2):
-	
-	im_2 = cv2.imread(im_2)
-	cv2.imshow('Saved Original', im_2)
-	im_2_edged = cv2.Canny(im_2, 230, 240)
-	cv2.imshow('Saved Edges', im_2_edged)
-	height_2, width_2 = im_2_edged.shape
-	im_2 = im_2_edged
-	pix_list_im_2 = []	
 
-	for i in range(1, height_2):
-		for j in range(1, width_2):
-			if im_2[i,j] != 0:
-				current_pix_im_2 = [i, j]
-				pix_list_im_2.append(current_pix_im_2)
-				# delete already drawn pixels
-				im_2[i, j] = 0
-				# make sure we're in the boundaries of the image
-				if ((i-1 > 0) and (j-1 > 0) and (i < height_2-1) and (j < width_2-1)):
-					# check neighborhood (3x3)
-					if (im_2[i-1,j-1]!=0 or im_2[i-1,j]!=0 or im_2[i-1,j+1]!=0 or \
-					im_2[i,j-1]!=0 or im_2[i,j+1]!=0 or im_2[i+1,j-1]!=0 or \
-					im_2[i+1,j]!=0 or im_2[i+1,j+1]!=0): 
-				  		# index through rows and columns of neighborhood matrix (3x3)
-						# current pixel here is image(k, l)
-						for k in range(i-1, i+2):
-							for l in range(j-1, j+2):
-								if im_2[k, l] != 0:
-									current_pix_im_2 = [k, l]
-									pix_list_im_2.append(current_pix_im_2)
-								        im_2[k, l] = 0
-									
-#	print "i = ", i
-#	print "j = ", j
-	print "length of pix_list = ", len(pix_list_im_2)
-#	print "pix_list = ", pix_list_im_2
-	for n in range(0, len(pix_list_im_2)):
-		px_input = pix_list_im_2[n][0]
-		py_input = pix_list_im_2[n][1]
-		pix_to_ik(px_input, py_input)
+def follow_pix(k, l):
 
+	# (n, o) first time through is the current pixel location (first pixel in line)
+	# index through rows and columns of neighborhood matrix (3x3)
+	# current pixel here is image(n, o)
+	global image
+	global pix_list
+	global pen_down
+	global current_pix
+	global height, width
 
+	for n in range(k-1, k+2):
+		for o in range(l-1, l+2):
+			if ((n < height) and (o < width)): 
+				if image[n, o] != 0:
+					pen_down = True
+					current_pix = [n, o, pen_down]
+					pix_list.append(current_pix)
+			        	image[n, o] = 0
+					follow_pix(current_pix[0], current_pix[1])
 
 
 
 #  function to convert a pixel coordinate into angles for the servos
 #  on each joint
-def pix_to_ik(px, py):
-	
+def pix_to_ik(px, py, pen_state):
+
+	global pix_list
+	global angle_array
+	global angles_df
+
 	# 1280 is width of image from pi cam, 232.72 is 2 inches (for offset) 
 	#  after multiplying by scalar
 	cx = (11.0 / 1280.0) * (px + 232.727272727)
@@ -130,7 +117,7 @@ def pix_to_ik(px, py):
 	theta_two = math.atan2(math.sqrt(1 - math.pow(alpha, 2)), alpha)
 	
 	# -90 <= theta_two <= +90
-	# ideall theta_one has the same constraints 
+	# theta_one has the same constraints 
 	if (theta_two < -(math.pi/2)):
 		theta_two += math.pi
 	elif (theta_two > (math.pi/2)):
@@ -141,18 +128,36 @@ def pix_to_ik(px, py):
 	gamma = math.atan2(k2, k1)
 	theta_one = math.atan2(cy, cx) - gamma
 	
+	if pen_state == True:
+		theta_three = 90
+	elif pen_state == False:
+		theta_three = 0
+
 	theta_one = round(math.degrees(theta_one))  # convert from rad to deg
 	theta_two = round(math.degrees(theta_two))  # convert from rad to deg
-	angles = [theta_one, theta_two]
+	angles = [theta_one, theta_two, theta_three]
 	angle_array.append(angles)	
-	return angles;
+	angles_df = pd.DataFrame(angle_array, columns = ['THETA1', 'THETA2', 'THETA3'])
+	angles_df += 90
+	return angles_df
 
 
 
 
-
-get_pix_saved_image('mountain_river.jpg')
-print "angle_array = ", (angle_array)
+find_pix()
+print "pix_list = ", pix_list
+print 
+print
+print
+print "angle_array = ", angle_array
+print
+print
+print
+print "angles_df = ", angles_df
+print 
+print
+print
+# get_pix_saved_image('mountain_river.jpg')
 # pix_to_ik(1280, 1024)
 
 cv2.waitKey(0)
