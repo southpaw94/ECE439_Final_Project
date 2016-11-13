25ca76fe35ff98c7e59a5b5a33a25ad7150106c4
Michael Estes <mjestes1994@gmail.com>
2016-11-13 13:11:39 -0500

    Python SQL code

---
+++ sql.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+def main():
+    
+    data =  [[i, i, i] for i in range(3)]
+    data = pd.DataFrame(data, columns=
+            ['THETA1', 'THETA2', 'THETA3'])
+
+    con = create_engine('mysql+mysqlconnector://root:passwd@localhost:3306/ECE_439')
+    data.to_sql(name='ANGLES', con=con, if_exists='replace', index_label='ID')
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
